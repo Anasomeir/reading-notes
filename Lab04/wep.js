b2337580cb6f25fb5d5f208c22848be608accf53
Anasomeir <anas.jawad.omair@gmail.com>
2021-03-04 16:29:27 +0200

    Messege

---
+++ Lab04/wep.js
@@ -0,0 +1,22 @@
+console.log("Odin");
+console.log(2022);
+console.log("Evan");
+alert ("Welcome")
+var score = prompt("Enter your score");
+if (score>6){
+    alert("Congradulation you Win");
+}
+else if (score<6){
+    alert("You lost");
+}
+
+
+var S = 3
+var X = 9
+console.log (S>X)
+console.log (S<X)
+console.log (S==X)
+
+
+document.write("Hello World!");
+document.write("<h1>Hello World!</h1><p>Have a nice day!</p>");
